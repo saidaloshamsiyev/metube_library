39f72f0632db45298385a6109c996c9b4490aee4
said4ik <muxammadaminartikov@gmail.com>
2024-09-26 16:53:53 +0500

    Initial commit

---
+++ src/main/java/metube/com/dto/BaseEntity.java
@@ -0,0 +1,35 @@
+package metube.com.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    protected UUID id;
+
+    @CreatedDate
+    protected LocalDateTime createdDate;
+
+
+    @UpdateTimestamp
+    protected LocalDateTime updatedDate;
+}
