7a5a775a87a92f9ffd585ef03ce44390f53372f3
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-10-23 15:36:05 +0500

    library added

--- src/main/java/metube/com/dto/response/UserResponse.java
+++ src/main/java/metube/com/dto/response/UserResponse.java
@@ -1,14 +1,12 @@
 package metube.com.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class UserResponse {
     private String username;
     private String email;
